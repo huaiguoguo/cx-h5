e0c6e0a57d0be5773c8bae12ec73be9de8ac7165
guofeng <guofeng@heiwangke.com>
2019-10-17 18:25:14 +0800

    sadf

--- pages/home/shop-xq.vue
+++ pages/home/shop-xq.vue
@@ -632,12 +632,14 @@ export default {
             justify-content: space-between;
             align-items: center;
             width: 100%;
-            height: 88px;
-            line-height: 88px;
+            // height: 88px;
+            // line-height: 88px;
             font-size: 36px;
             font-weight: normal;
             font-stretch: normal;
             color: #333;
+            box-shadow: 0px 20px 30px -10px rgba(51, 51, 51, 0.1);
+            padding: 35px 20px 20px 20px;
             .goback {
                 width: 16px;
                 // height: 32px;
